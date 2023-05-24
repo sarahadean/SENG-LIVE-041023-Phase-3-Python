af8d9f9e5e4c45b3303eb4cb6a102e8d144de69c
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-05-24 14:57:53 -0400

    oop

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -5,13 +5,50 @@
     # Compare the pet instances to demonstrate they are not the same object
     # Note: add 'pass' to the pet class 
 
+# import ipdb
 
-class Pet:
-    # pass
+#1. define a pet class
+#class keyword
+# class Pet: #UpperCamelCase #PascalCase
+
+#     pass
+
+# ipdb.set_trace()
+
+# baboi = Pet()  # instantiate a new instance 
+# fiona = Pet()
+# beau = Pet()
+# winnie = Pet()
+# chloe = Pet()
+# moseph = Pet()
+# beau            # calling the instance  
+# <__main__.Pet object at 0x106c61160> #instance detail 
+
+# ipdb> winnie == chloe
+# False
+
+# winnie and chloe are not the same. They are different objects in memory 
+# they are coming the same source, Pet class
+
+
 # 3. ✅ Demonstrate __init__ 
     # Add arguments to instances  
     # use dot notation to access their attributes 
     # update attributes with new values 
+
+class Pet:
+    def __init__(self): # initialization method
+        # to provide objects(instances) with unique attributes upon the instantiation
+        print(self)
+    pass
+
+baboi = Pet()
+
+
+# what is self? 
+    # self => instance of Pet class
+    # <__main__.Pet object at 0x100e17910>
+    # we know that the self is `baboi`, exactly same as the instance  
 
 
 # 4.✅ Demonstrate instance methods by creating a print_pet_details function that will print the pet attributes
